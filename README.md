71f6cc17fc0c6650cd80f0d2d5e475f97cc9b8d9
Leigh Hunt <leigh@venari.co.nz>
2019-04-15 20:44:50 +1200

    kicks off performance issues - CPU at 60% (instead of 0%, and memory slowly climbing).

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # mem-leak-test
 
 ```
-docker run -p 80:3000 leighghunt/mem-leak-test
+docker run -p 80:3000 --device=/dev/ttyAMA0 leighghunt/mem-leak-test
 ```
--- app.js
+++ app.js
@@ -10,5 +10,16 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, () => {
   console.log(`Server running port - using multistage build on node 11: ${port}/`);
-  console.log(`Adding serialport package, and bunch of npm stuff, but not calling serialport`);
+  console.log(`Adding serialport package, and bunch of npm stuff, and now calling serialport`);
 });
+
+
+
+var SerialPort = require("serialport");
+
+console.log("About to open serial port /dev/ttyAMA0" );
+var serialPort = new SerialPort('/dev/ttyAMA0', {
+  baudRate: 57600,
+});
+var Readline = SerialPort.parsers.Readline;
+var parser = serialPort.pipe(new Readline({delimiter: '\r\n'}));
